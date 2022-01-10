e6b2fbb0e0f12d2a04dc9901a30755b520c39538
alex-skxy <alex13skx@gmail.com>
2022-01-10 14:08:05 +0100

    add keyboard shortcut and update version

--- src/firefox/background.js
+++ src/firefox/background.js
@@ -1,13 +1,22 @@
 (function () {
     'use strict';
+
     function closeTabsByKeyword(keyword) {
         if (!keyword) {
-            return;
+            chrome.tabs.query({}, function (tabs) {
+                tabs.filter(t => t.url === 'about:newtab').forEach(t => chrome.tabs.remove(t));
+            });
+        } else {
+            chrome.tabs.query({}, function (tabs) {
+                tabs.filter(t => t.url.contains(keyword) || tab.title.contains(keyword)).forEach(t => chrome.tabs.remove(t));
+            });
         }
 
         chrome.tabs.query({}, function (tabs) {
             for (const tab of tabs) {
-                if (tab.url.indexOf(keyword) !== -1) {
+                console.log(tab);
+                if (tab.url.match(keyword) || tab.title.match(keyword)) {
+                    console.log(tab);
                     chrome.tabs.remove(tab.id);
                 }
             }
